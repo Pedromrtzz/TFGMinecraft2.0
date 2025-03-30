3c4ab5f10369eb0fbca8c83c2aa4bacc92b7bfad
Pedromrtz <pmartinezigual@gmail.com>
2025-03-30 16:55:21 +0200

    Caldo Pescado
    Item, Comida y Crafteo metido dentro del juego

--- TFGMinecraft/src/main/java/com/pedromrtz/tfgmod/Item/ModFoodPropierties.java
+++ TFGMinecraft/src/main/java/com/pedromrtz/tfgmod/Item/ModFoodPropierties.java
@@ -26,5 +26,8 @@ public class ModFoodPropierties {
     public static final FoodProperties RAMEN_CARNE = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2F)
             .build();
 
+    public static final FoodProperties CALDO_PESCADO = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2F)
+            .build();
+
 
 }
