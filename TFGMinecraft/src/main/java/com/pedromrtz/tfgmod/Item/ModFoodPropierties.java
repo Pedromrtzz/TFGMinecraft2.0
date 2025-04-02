19926a1a6fe6a2b7825ed2f304ef313f1d9e1833
Pedromrtz <pmartinezigual@gmail.com>
2025-04-02 12:14:30 +0200

    Arroz Frito
    Item, Crafteo y Comida metido dentro del juego

--- TFGMinecraft/src/main/java/com/pedromrtz/tfgmod/Item/ModFoodPropierties.java
+++ TFGMinecraft/src/main/java/com/pedromrtz/tfgmod/Item/ModFoodPropierties.java
@@ -92,4 +92,7 @@ public class ModFoodPropierties {
 
     public static final FoodProperties ARROZ_VERDURAS = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2F)
             .build();
+
+    public static final FoodProperties ARROZ_FRITO = new FoodProperties.Builder().nutrition(2).saturationModifier(0.2F)
+            .build();
 }
